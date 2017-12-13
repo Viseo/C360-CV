527968e0c7135366ff20a469a34dc60be52e0aba
Unknown <guyuanhao@foxmail.com>
2017-12-13 11:54:43 +0100

    combine spring data with hibernate

--- cv-backend/src/main/java/com/viseo/c360/cv/repositories/AccountDAO.java
+++ cv-backend/src/main/java/com/viseo/c360/cv/repositories/AccountDAO.java
@@ -3,128 +3,13 @@ package com.viseo.c360.cv.repositories;
 import com.viseo.c360.cv.models.entities.UsersEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
-
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
+
 
 
 @Transactional
 @Component
-public class AccountDAO implements CrudRepository<UsersEntity, Long> {
+public interface AccountDAO extends CrudRepository<UsersEntity, Long> {
 
-    @PersistenceContext
-    EntityManager em;
 
-    public UsersEntity findByCredential(String mail, String password){
-        try{
-            return em.createQuery("SELECT U FROM UsersEntity U JOIN FETCH U.missions M " +
-                    "JOIN FETCH U.languages " +
-                    "JOIN FETCH M.skills " +
-                    "WHERE U.mail = ?1 AND U.password = ?2",UsersEntity.class)
-                    .setParameter(1,mail)
-                    .setParameter(2,password)
-                    .getSingleResult();
-        }
-        catch (NoResultException nre){
-            //Ignore this because as per our logic this is ok!
-        }
-        return null;
-    }
-
-    public UsersEntity findByMail(String mail){
-        try{
-            return  em.createQuery("SELECT U FROM UsersEntity U JOIN FETCH U.missions M " +
-                    "JOIN FETCH U.languages " +
-                    "JOIN FETCH M.skills WHERE U.mail = ?1", UsersEntity.class)
-                    .setParameter(1, mail).getSingleResult();
-        }
-        catch (NoResultException nre){
-            //Ignore this because as per our logic this is ok!
-        }
-        return null;
-    }
-
-    public UsersEntity findById(int id){
-        try{
-            return em.createQuery("SELECT U FROM UsersEntity U JOIN FETCH U.missions M " +
-                    "JOIN FETCH U.languages " +
-                    "JOIN FETCH M.skills WHERE U.id = ?1", UsersEntity.class)
-                    .setParameter(1,id).getSingleResult();
-        }
-        catch (NoResultException nre){
-            //Ignore this because as per our logic this is ok!
-        }
-        return null;
-    }
-
-    public List<UsersEntity> getAll(){
-        try{
-            return em.createQuery("SELECT U FROM UsersEntity U JOIN FETCH U.missions M " +
-                    "JOIN FETCH U.languages " +
-                    "JOIN FETCH M.skills", UsersEntity.class).getResultList();
-        }
-        catch (NoResultException nre){
-            //Ignore this because as per our logic this is ok!
-        }
-        return null;
-    }
-
-    @Override
-    public <S extends UsersEntity> S save(S entity) {
-        return null;
-    }
-
-    @Override
-    public <S extends UsersEntity> Iterable<S> saveAll(Iterable<S> entities) {
-        return null;
-    }
-
-    @Override
-    public Optional<UsersEntity> findById(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public boolean existsById(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public Iterable<UsersEntity> findAll() {
-        return null;
-    }
-
-    @Override
-    public Iterable<UsersEntity> findAllById(Iterable<Long> longs) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(Long aLong) {
-
-    }
-
-    @Override
-    public void delete(UsersEntity entity) {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends UsersEntity> entities) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
 }
--- cv-backend/src/main/java/com/viseo/c360/cv/services/AccountServiceImpl.java
+++ cv-backend/src/main/java/com/viseo/c360/cv/services/AccountServiceImpl.java
@@ -9,6 +9,9 @@ import com.viseo.c360.cv.repositories.AccountDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
 import java.util.Date;
 import java.util.List;
 
@@ -17,13 +20,61 @@ import static org.springframework.util.CollectionUtils.isEmpty;
 @Service
 public class AccountServiceImpl implements AccountService {
 
+    @PersistenceContext
+    EntityManager em;
+
     @Autowired
     private AccountDAO accountDAO;
+
+    public UsersEntity findByCredential(String mail, String password){
+        try{
+            return em.createQuery("SELECT U FROM UsersEntity U JOIN FETCH U.missions M " +
+                    "JOIN FETCH U.languages " +
+                    "JOIN FETCH M.skills " +
+                    "WHERE U.mail = ?1 AND U.password = ?2",UsersEntity.class)
+                    .setParameter(1,mail)
+                    .setParameter(2,password)
+                    .getSingleResult();
+        }
+        catch (NoResultException nre){
+            //Ignore this because as per our logic this is ok!
+        }
+        return null;
+    }
+
+    public UsersEntity findByMail(String mail){
+        try{
+            return  em.createQuery("SELECT U FROM UsersEntity U JOIN FETCH U.missions M " +
+                    "JOIN FETCH U.languages " +
+                    "JOIN FETCH M.skills WHERE U.mail = ?1", UsersEntity.class)
+                    .setParameter(1, mail).getSingleResult();
+        }
+        catch (NoResultException nre){
+            //Ignore this because as per our logic this is ok!
+        }
+        return null;
+    }
+
+    public UsersEntity findById(int id){
+        try{
+            return em.createQuery("SELECT U FROM UsersEntity U JOIN FETCH U.missions M " +
+                    "JOIN FETCH U.languages " +
+                    "JOIN FETCH M.skills WHERE U.id = ?1", UsersEntity.class)
+                    .setParameter(1,id).getSingleResult();
+        }
+        catch (NoResultException nre){
+            //Ignore this because as per our logic this is ok!
+        }
+        return null;
+    }
+
+
+
 
     @Override
     public UsersEntity exist(String mail, String password) {
 
-        return accountDAO.findByCredential(mail, password);
+        return this.findByCredential(mail, password);
     }
 
     @Override
@@ -33,16 +84,24 @@ public class AccountServiceImpl implements AccountService {
     }
 
     public UsersEntity mailExist(String mail) {
-        return accountDAO.findByMail(mail);
+        return this.findByMail(mail);
     }
 
     public UsersEntity getUserById(int id) {
-        return accountDAO.findById(id);
+        return this.findById(id);
     }
 
     @Override
-    public List<UsersEntity> getAll() {
-        return accountDAO.getAll();
+    public List<UsersEntity> getAll(){
+        try{
+            return em.createQuery("SELECT U FROM UsersEntity U JOIN FETCH U.missions M " +
+                    "JOIN FETCH U.languages " +
+                    "JOIN FETCH M.skills", UsersEntity.class).getResultList();
+        }
+        catch (NoResultException nre){
+            //Ignore this because as per our logic this is ok!
+        }
+        return null;
     }
 
     public UsersEntity updateUser(UsersEntity user){
