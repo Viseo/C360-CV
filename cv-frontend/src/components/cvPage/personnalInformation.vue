913ebdc1c4ad3d3c0ea20219444aec721e440c4a
Unknown <guyuanhao@foxmail.com>
2018-01-18 10:54:07 +0100

    change form alert mode

--- cv-frontend/src/components/cvPage/personnalInformation.vue
+++ cv-frontend/src/components/cvPage/personnalInformation.vue
@@ -12,9 +12,10 @@
       <label v-bind:style = "stylep">Prénom</label>
       <div class="inputWithPicto">
         <i class="fa fa-user-o fa-2x picto" aria-hidden="true"></i>
-        <input class="inputInfo" name = "firstName" v-on:keyup="verificationChar" maxlength="125" type="text" placeholder="Prenom"
+        <input class="inputInfo" name = "firstName" maxlength="125" type="text" placeholder="Prenom"
                v-model="infoPerso.firstName">
-        <span id="alM" style="opacity: 0; font-size: 10px; display: none">Veuillez entrer un prénom valide</span>
+        <span style="color:red;font-weight:bold;"
+              v-show="infoPerso.firstName==''">Prénom invalide</span>
       </div>
     </div>
 
@@ -22,9 +23,10 @@
       <label v-bind:style = "stylep">Nom</label>
       <div class="inputWithPicto">
         <i class="fa fa-user-o fa-2x picto" aria-hidden="true"></i>
-        <input class="inputInfo" name="name" v-on:keyup="verificationChar" @input="infoPerso.lastName=infoPerso.lastName.toUpperCase()" placeHolder="Nom"
+        <input class="inputInfo" name="name" @input="infoPerso.lastName=infoPerso.lastName.toUpperCase()" placeHolder="Nom"
                v-model="infoPerso.lastName">
-        <span id="alM1" style="opacity: 0; font-size: 10px; display: none">Veuillez entrer un nom valide</span>
+        <span style="color:red;font-weight:bold;"
+              v-show="infoPerso.lastName==''">Nom invalide</span>
       </div>
     </div>
 
@@ -33,6 +35,8 @@
       <div class="inputWithPicto">
         <i class="fa fa-birthday-cake fa-2x picto" aria-hidden="true"></i>
         <input class="inputInfo date" name="birth" type="date" v-model="infoPerso.birth_date">
+        <span style="color:red;font-weight:bold;"
+              v-show="infoPerso.birth_date == ''">Age invalide</span>
         <span><i id="calendar_icon" class="fa fa-calendar pictoInput" aria-hidden="true"></i> </span>
       </div>
     </div>
@@ -41,9 +45,10 @@
       <label v-bind:style = "stylep">Fonction</label>
       <div class="inputWithPicto">
         <i class="fa fa-tag fa-2x picto" aria-hidden="true"></i>
-        <input class="inputInfo" name="fonction" v-on:keyup="verificationChar" maxlength="125" placeHolder="Poste"
+        <input class="inputInfo" name="fonction" maxlength="125" placeHolder="Poste"
                v-model="infoPerso.position">
-        <span id="alM2" style="opacity: 0; font-size: 10px; display: none">Veuillez entrer une fonction valide</span>
+        <span style="color:red;font-weight:bold;"
+              v-show="infoPerso.position == ''">Fonction invalide</span>
       </div>
     </div>
 
@@ -51,9 +56,10 @@
       <label v-bind:style = "stylep">Expérience (en année)</label>
       <div class="inputWithPicto">
         <i  class="fa fa-tachometer fa-2x picto" aria-hidden="true"></i>
-        <input class="inputInfo" name="experience" v-on:keyup="verificationChar" placeHolder="Années d'expérience"
+        <input class="inputInfo" name="experience" placeHolder="Années d'expérience"
                v-model="infoPerso.experience">
-        <span id="alM5" style="opacity: 0; font-size: 10px; display: none">Veuillez entrer un nombre d'années valide</span>
+        <span style="color:red;font-weight:bold;"
+              v-show="infoPerso.experience == '' || !isFinite(infoPerso.experience)">Nombre d'années invalide</span>
       </div>
     </div>
 
@@ -61,9 +67,8 @@
       <label v-bind:style = "stylep">Email</label>
       <div class="inputWithPicto">
         <i  class="fa fa-envelope-o fa-2x picto" aria-hidden="true"></i>
-        <input class="inputInfo" name="email" type="email" v-on:keyup="verificationEmail" placeHolder="mail@viseo.fr"
-               v-model="infoPerso.mail">
-        <span id="alM6" style="opacity: 0; font-size: 10px; display: none">Veuillez entrer une adresse email valide</span>
+        <input class="inputInfo" name="email" type="email"
+               v-model="infoPerso.mail" disabled>
       </div>
     </div>
 
@@ -72,6 +77,7 @@
       <div class="inputWithPicto">
         <i class="fa fa-phone fa-2x picto" aria-hidden="true"></i>
         <input class="inputInfo" name="telephone" placeholder="tel" v-model="infoPerso.telephone">
+        <span style="color:red;font-weight:bold;" v-show="infoPerso.telephone == ''">Téléphone invalide</span>
         <img v-bind:src="pictoTelSrc" :style="styleTel" class="pictoInput">
       </div>
     </div>
@@ -83,7 +89,6 @@
         <i class="fa fa-language fa-2x picto" aria-hidden="true"></i>
         <vSelect multiple v-model="infoPerso.languages" taggable
                  :options="languages" class="selectorStyle"></vSelect>
-        <span id="alM3" style="opacity: 0; font-size: 10px; display: none">Veuillez entrer une langue valide</span>
       </div>
     </div>
 
@@ -91,9 +96,8 @@
       <label v-bind:style = "stylep">Hobbies</label>
       <div class="inputWithPicto">
         <i class="fa fa-gamepad fa-2x picto" aria-hidden="true"></i>
-        <input class="inputInfo" name="hobbies" v-on:keyup="verificationChar" maxlength="50" placeHolder="hobbies"
+        <input class="inputInfo" name="hobbies" maxlength="50" placeHolder="hobbies"
                v-model="infoPerso.hobbies">
-        <span id="alM4" style="opacity: 0; font-size: 10px; display: none">Veuillez entrer un hobbie valide</span>
       </div>
     </div>
   </div>
@@ -142,109 +146,6 @@ import vSelect from 'vue-select'
         text.style.opacity = 0;
         text.style.zIndex = -2;
       },
-      verificationChar: function () {
-        let toShow = false;
-        for(let letter of event.target.value){
-          if(!((letter.charCodeAt(0)>=97 && letter.charCodeAt(0)<=122) ||(letter.charCodeAt(0)>=65 && letter.charCodeAt(0)<=90)||
-            letter.charCodeAt(0)==32 || letter.charCodeAt(0)==39 || letter.charCodeAt(0)==46 || letter.charCodeAt(0)==45||
-            (letter.charCodeAt(0)>=232 && letter.charCodeAt(0)<=235))){
-            toShow = true;
-          }
-        }
-        let selectedInput="";
-        switch (event.target.name){
-          case "firstName":
-            selectedInput = "alM";
-            break;
-          case "name":
-            selectedInput ="alM1";
-            break;
-          case "fonction":
-            selectedInput ="alM2";
-            break;
-          case "languages":
-            selectedInput ="alM3";
-            break;
-          case "hobbies":
-            selectedInput ="alM4";
-            break;
-          case "experience":
-            selectedInput ="alM5";
-            break;
-        }
-        let alertMessage = document.getElementById(selectedInput);
-        if(toShow==false || event.target.name == "experience"){
-          alertMessage.style.display="none";
-        }else{
-          alertMessage.style.display="block";
-          alertMessage.style.opacity=1;
-          alertMessage.style.color="rgb(255,0,0)";
-          alertMessage.style.textAlign= "center";
-        }
-        if(event.charCode==0){
-          this.validateForm();
-        }
-      },
-      verificationEmail: function () {
-        let toShow= true;
-        for(let letter of event.target.value){
-          if(letter.charCodeAt(0)==64)toShow=false;
-        }
-        let alertMessage = document.getElementById("alM6");
-        if(toShow){
-          alertMessage.style.display="block";
-          alertMessage.style.opacity=1;
-          alertMessage.style.color="rgb(255,0,0)";
-          alertMessage.style.textAlign= "center";
-        }else{
-          alertMessage.style.display="none";
-        }
-      },
-      validateForm:function () {
-        let toExperienceInput = () =>{
-          let toShow = true;
-          for(let letter of event.target.value){
-            if(letter.charCodeAt(0)>=48 && letter.charCodeAt(0)<=57){
-              toShow = false;
-            }
-          }
-          return toShow;
-        };
-        let selectedInput="";
-        switch (event.target.name){
-          case "firstName":
-            selectedInput = "alM";
-            break;
-          case "name":
-            selectedInput ="alM1";
-            break;
-          case "fonction":
-            selectedInput ="alM2";
-            break;
-          case "hobbies":
-            selectedInput = "alM4";
-            break;
-          case "languages":
-            selectedInput ="alM3";
-            break;
-          case "experience":
-            selectedInput ="alM5";
-            break;
-        }
-        let alertMessage = document.getElementById(selectedInput);
-        if(((event.target.value.length<2 || event.target.value.length>50) &&
-          (event.target.name =="langues" || event.target.name == "hobbies"))||
-          (event.target.name=="experience" && (toExperienceInput() ||
-          (event.target.value.length<2  || event.target.value.length>50)))||
-          ((event.target.value.length<2 || event.target.value.length>125) &&
-          (event.target.name =="prenom" || event.target.name == "nom" || event.target.name == "fonction"))){
-
-          alertMessage.style.display="block";
-          alertMessage.style.opacity=1;
-          alertMessage.style.color="rgb(255,0,0)";
-          alertMessage.style.textAlign= "center";
-        }
-      }
     },
     data: function () {
       return {
